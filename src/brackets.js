075119367074500b9701565def423f098a7556aa
GephestUA <66007432+GephestUA@users.noreply.github.com>
2021-05-31 14:04:10 +0300

    Update brackets.js

--- src/brackets.js
+++ src/brackets.js
@@ -19,3 +19,31 @@ function bracketsFilter(brackets) {
 }
 
 console.log(bracketsFilter("(())"));
+
+function bracketsFilterBig(data) {
+  let bracketsArr = [];
+  let obj = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+  };
+
+  for (let index = 0; index < data.length; index++) {
+    let brackets = data[index];
+    if (brackets === "(" || brackets === "[" || brackets === "{") {
+      bracketsArr.push(brackets);
+    } else {
+      let lastIndex = bracketsArr.pop();
+
+      if (brackets !== obj[lastIndex]) {
+        return false;
+      }
+    }
+  }
+  if (bracketsArr.length) {
+    return false;
+  }
+  return true;
+}
+
+console.log(bracketsFilterBig("{(())}"));
